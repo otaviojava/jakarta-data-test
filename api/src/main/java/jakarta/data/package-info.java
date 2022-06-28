8a8e6f8260498758e895ee1131f8090f70757c76
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-28 17:25:46 +0100

    Update package info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/package-info.java
+++ api/src/main/java/jakarta/data/package-info.java
@@ -18,69 +18,6 @@
  */
 
 /**
- * Configuration for Java MicroProfile
  *
- * <h2>Rationale</h2>
- *
- * <p>
- * For many project artifacts (e.g. WAR, EAR) it should be possible to build them only once and then install them at
- * different customers, stages, etc. They need to target those different execution environments without the necessity of
- * any repackaging. In other words: depending on the situation they need different configuration.
- *
- * <p>
- * This is easily achievable by having a set of default configuration values inside the project artifact. But be able to
- * overwrite those default values from external.
- *
- * <h2>How it works</h2>
- *
- * <p>
- * A <em>Configuration</em> consists of the information collected from the registered
- * {@link org.eclipse.microprofile.config.spi.ConfigSource ConfigSources}. These {@code ConfigSources} get sorted
- * according to their <i>ordinal</i>. That way it is possible to overwrite configuration with lower importance from
- * outside.
- *
- * <p>
- * By default there are 3 ConfigSources:
- *
- * <ul>
- * <li>{@code System.getProperties()} (ordinal=400)</li>
- * <li>{@code System.getenv()} (ordinal=300)</li>
- * <li>all {@code META-INF/microprofile-config.properties} files on the ClassPath. (ordinal=100, separately configurable
- * via a config_ordinal property inside each file)</li>
- * </ul>
- *
- * <p>
- * That means that one can put the default configuration in a {@code META-INF/microprofile-config.properties} anywhere
- * on the classpath and the Operations team can later simply e.g set a system property to change this default
- * configuration.
- *
- * <p>
- * It is of course also possible to register own {@link org.eclipse.microprofile.config.spi.ConfigSource ConfigSources}.
- * A {@code ConfigSource} could e.g. read configuration values from a database table, a remote server, etc
- *
- * <h2>Accessing and Using the Configuration</h2>
- *
- * <p>
- * The configuration of an application is represented by an instance of {@link org.eclipse.microprofile.config.Config}.
- * The {@link org.eclipse.microprofile.config.Config} can be accessed via the
- * {@link org.eclipse.microprofile.config.ConfigProvider}.
- *
- * <pre>
- * Config config = ConfigProvider.getConfig();
- * String restUrl = config.getValue("myproject.some.endpoint.url", String.class);
- * </pre>
- *
- * <p>
- * Injection via a JSR-330 DI container is also supported:
- *
- * <pre>
- * &#064;Inject
- * &#064;ConfigProperty(name="myproject.some.endpoint.url");
- * private String restUrl;
- * </pre>
- *
- * @author <a href="emijiang@uk.ibm.com">Emily Jiang</a>
- * @author <a href="mailto:struberg@apache.org">Mark Struberg</a>
  */
-@org.osgi.annotation.versioning.Version("3.0.0")
 package jakarta.data;
