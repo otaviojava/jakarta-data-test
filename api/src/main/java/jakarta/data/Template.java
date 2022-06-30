08f71c54005a4898dba245400e5806136e4fd102
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-30 12:36:23 +0100

    improve documentation on templat einteface
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/Template.java
+++ api/src/main/java/jakarta/data/Template.java
@@ -100,7 +100,6 @@ public interface Template {
      * @param <K>         the id type
      * @return the entity instance otherwise {@link Optional#empty()}
      * @throws NullPointerException when either the entityClass or id are null
-     * @throws IdNotFoundException  when the entityClass does not have the Id annotation
      */
     <T, K> Optional<T> find(Class<T> entityClass, K id);
 
@@ -112,7 +111,6 @@ public interface Template {
      * @param <T>         the entity class type
      * @param <K>         the id type
      * @throws NullPointerException when either the entityClass or id are null
-     * @throws IdNotFoundException  when the entityClass does not have the Id annotation
      */
     <T, K> void delete(Class<T> entityClass, K id);
 }
