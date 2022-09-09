d17251f1cb7c1d0b237c6d50fa97d4cc7e438548
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-09 10:34:38 +0100

    Rename data to CrudRepository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/repository/CRUDRepository.java
+++ api/src/main/java/jakarta/data/repository/CRUDRepository.java
@@ -44,7 +44,7 @@ import java.util.Optional;
  * @param <T>  the bean type
  * @param <K> the K type
  */
-public interface DataRepository<T, K> {
+public interface CRUDRepository<T, K> {
 
     /**
      * Saves entity
