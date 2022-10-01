4e4deeadd518f0bbee968f139091dbdd0b724321
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-01 15:43:24 +0100

    make module-info open
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module jakarta.data.api {
+open module jakarta.data.api {
     exports jakarta.data;
     exports jakarta.data.repository;
     uses jakarta.data.repository.Order.OrderSupplier;
