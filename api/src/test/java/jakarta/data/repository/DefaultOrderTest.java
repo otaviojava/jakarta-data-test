a5a7c950cfc95e426074718a685eae07756a0680
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-01 08:31:36 +0100

    fixes imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/test/java/jakarta/data/repository/DefaultOrderTest.java
+++ api/src/test/java/jakarta/data/repository/DefaultOrderTest.java
@@ -20,8 +20,6 @@ package jakarta.data.repository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DefaultOrderTest {
 
     @Test
