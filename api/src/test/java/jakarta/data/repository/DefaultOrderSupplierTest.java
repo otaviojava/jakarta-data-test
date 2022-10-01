2b1f9f120a4ead9b2e8843314c7f83defdef3a69
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-01 11:33:09 +0100

    Fixe import
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/test/java/jakarta/data/repository/DefaultOrderSupplierTest.java
+++ api/src/test/java/jakarta/data/repository/DefaultOrderSupplierTest.java
@@ -21,8 +21,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DefaultOrderSupplierTest {
 
     private Order.OrderSupplier supplier;
