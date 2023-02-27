73f58f928ef1a0519a0e5f966096aa71c041a394
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-27 06:32:03 +0000

    create book repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- nosql/src/main/java/expert/os/jakarta/data/nosql/Book.java
+++ nosql/src/main/java/expert/os/jakarta/data/nosql/Book.java
@@ -1,2 +1,20 @@
-package expert.os.jakarta.data.nosql;public class Book {
+package expert.os.jakarta.data.nosql;
+
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+@Entity
+public class Book {
+
+
+    @Id
+    private String isbn;
+
+    @Column
+    private String title;
+
+    @Column
+    private int edition;
 }
