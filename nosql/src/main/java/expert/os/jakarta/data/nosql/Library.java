19d1156db144ae7c3bc2ac871613b0ebe4812157
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-27 06:34:22 +0000

    Create Library as book repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- nosql/src/main/java/expert/os/jakarta/data/nosql/Library.java
+++ nosql/src/main/java/expert/os/jakarta/data/nosql/Library.java
@@ -1,4 +1,8 @@
 package expert.os.jakarta.data.nosql;
 
-public interface BookRepository {
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Repository;
+
+@Repository
+public interface Library extends CrudRepository<String, Book> {
 }
